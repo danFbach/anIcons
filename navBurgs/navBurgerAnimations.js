7ca8c7581c248340fa48c47fd95c38e19586af13
danFbach <quiktekrepair@gmail.com>
2016-10-27 21:06:14 -0500

    added search and first burger

---
+++ navBurgs/navBurgerAnimations.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	/*$(window).load(function() {
+	  $("body").removeClass("preload");
+	});*/
+	$('.navBurger').click(function(){
+		$('.navBurger span').toggleClass('active');
+	});
+});
