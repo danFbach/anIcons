1d3ba5263f584b32eafde02fe5ea2f584d045d2f
danFbach <quiktekrepair@gmail.com>
2016-10-28 22:52:39 -0500

    added crushing burger with spin

--- navBurgs/navBurgerAnimations.js
+++ navBurgs/navBurgerAnimations.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
-/*	
+	
 //JS-0
 	$('.navBurger').click(function(){
 		$('body').removeClass('preload');
 		$('.navBurger span').toggleClass('active');
 	});	
-*/	
+	
 /*
 //JS-1
 	$('.navBurger').click(function(){
