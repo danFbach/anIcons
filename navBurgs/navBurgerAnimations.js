306449051f38e498669483e42879e0ff9f741559
danFbach <quiktekrepair@gmail.com>
2016-10-28 22:16:23 -0500

    added 2 new search icons, one profile animations and 3 patty bounce

--- navBurgs/navBurgerAnimations.js
+++ navBurgs/navBurgerAnimations.js
@@ -1,8 +1,23 @@
 $(document).ready(function(){
-	/*$(window).load(function() {
-	  $("body").removeClass("preload");
-	});*/
+/*	
+//JS-0
+	$('body').removeClass('preload');
 	$('.navBurger').click(function(){
 		$('.navBurger span').toggleClass('active');
 	});
+*/	
+	
+//JS-1
+	$('.navBurger').click(function(){
+		$('body').removeClass('preload');
+		$('.navBurger span').toggleClass('active');
+	});
+	
+/*
+//JS-2
+	$('.navBurger').click(function(){
+		$('.navBurger').toggleClass('active');
+		$('body').removeClass('preload');
+	});
+	*/
 });
