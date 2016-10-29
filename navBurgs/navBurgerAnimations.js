db2d6d86f592eba6624a494ab8cea18c85ec13fb
danFbach <quiktekrepair@gmail.com>
2016-10-28 22:27:30 -0500

    small bug fix with js activation

--- navBurgs/navBurgerAnimations.js
+++ navBurgs/navBurgerAnimations.js
@@ -1,23 +1,16 @@
 $(document).ready(function(){
 /*	
 //JS-0
-	$('body').removeClass('preload');
-	$('.navBurger').click(function(){
-		$('.navBurger span').toggleClass('active');
-	});
-*/	
-	
-//JS-1
 	$('.navBurger').click(function(){
 		$('body').removeClass('preload');
 		$('.navBurger span').toggleClass('active');
-	});
-	
+	});	
+*/	
 /*
-//JS-2
+//JS-1
 	$('.navBurger').click(function(){
 		$('.navBurger').toggleClass('active');
 		$('body').removeClass('preload');
 	});
-	*/
+*/
 });
