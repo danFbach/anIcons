f24bcdf17c32b20b6bc8b812637e7a225818f108
lemonSkunnk <quiktekrepair@gmail.com>
2017-12-14 08:50:11 -0600

    restyled for easier viewing, simplified js

--- navBurgs/navBurgerAnimations.js
+++ navBurgs/navBurgerAnimations.js
@@ -1,6 +1,17 @@
 $(document).ready(function(){
 
+    $('.anicon').click(function() {
+        $(this).removeClass('preload');
+        if ($(this).children().hasClass('navStack7')) {
+            $(this).children().toggleClass('active');
+        } else {
+            var kids = $(this).children().children();
+            $(kids).toggleClass('active');
+        }
+    });
+
 //JS-0 **Default**
+
 	$('.navBurger').click(function(){
 		$('body').removeClass('preload');
 		$('.navBurger span').toggleClass('active');
